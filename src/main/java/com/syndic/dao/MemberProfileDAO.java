da6eba5917e4683b758b7b71e2be26133ef76dab
ASMAE <asmae_karmouchi@um5.ac.ma>
2024-05-25 19:00:23 +0200

    Initial commit

---
+++ src/main/java/com/syndic/dao/MemberProfileDAO.java
@@ -0,0 +1,20 @@
+package com.syndic.dao;
+
+import com.syndic.beans.User;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.syndic.beans.Member;
+
+public interface MemberProfileDAO {
+
+    void addMember(Member member) throws SQLException ;
+
+    Member getMemberByUserId(int userId) throws SQLException;
+
+    void updateMember(Member member) throws SQLException;
+
+    List<Member> getMember();
+    int getMemberCount() throws SQLException;
+}
+
